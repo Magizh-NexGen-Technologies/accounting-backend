7d9de0732f398c0d6d43e8d2a30ea2bbe50cbc54
Magizh NexGen Technologies <udhay@mntfuture.com>
2025-06-10 13:29:28 +0530

    Update connectDB.js

--- utils/config/connectDB.js
+++ utils/config/connectDB.js
@@ -4,7 +4,10 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-}); 
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 pool.connect()
   .then(() => console.log('✅ PostgreSQL connected successfully'))
